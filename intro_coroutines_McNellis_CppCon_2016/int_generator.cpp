1fb6a7d1bdcd325986996d0e42d1e52e3964f9dd
tal <tal.lancaster+gh@gmail.com>
2017-07-03 18:00:54 -0600

    intro_coroutines_McNellis_CppCon_2016/int_generator.cpp: working generator example

---
+++ intro_coroutines_McNellis_CppCon_2016/int_generator.cpp
@@ -0,0 +1,96 @@
+
+/**
+ * generator example: int_generator
+ * From James McNellis: Introduction to C++ Coroutines CppCon 2016.
+ *
+ * compile: clang++ -std=c++14 -fcoroutines-ts int_generator.cpp
+ * Tal Lancaster 2017-07-01
+ **/
+
+#include <iostream>
+#include <experimental/coroutine>
+
+struct int_generator {
+    struct promise_type {
+        int_generator get_return_object () {
+            //return int_generator (std::experimental::coroutine_handle <promise_type>::from_promise (*this) );
+            return int_generator (this);
+        }
+
+        auto initial_suspend() { return std::experimental::suspend_always {}; }
+        auto final_suspend() { return std::experimental::suspend_always {}; }
+
+        void unhandled_exception() { std::terminate(); }
+        void return_void() {}
+
+        auto yield_value (int const& value) {
+            _current = &value;
+            return std::experimental::suspend_always {};
+        }
+
+        int const* _current;
+    };
+
+    int_generator (int_generator&& rhs)
+    : _coroutine {rhs._coroutine}
+    {
+        rhs._coroutine = nullptr;
+    }
+
+    struct iterator: std::iterator <std::input_iterator_tag, int>
+    {
+        iterator () = default;
+        iterator (std::experimental::coroutine_handle <promise_type> coroutine)
+        : _coroutine {coroutine}
+        { }
+
+        iterator& operator++()
+        {
+            _coroutine.resume ();
+            if (_coroutine.done () ) { _coroutine = nullptr; }
+            return *this;
+        }
+
+        int const& operator*() const {
+            return *_coroutine.promise ()._current;
+        }
+
+        bool operator!=(iterator const &_Right) const { return (_coroutine != _Right._coroutine); }
+
+        std::experimental::coroutine_handle <promise_type> _coroutine;
+    };
+
+    iterator begin () {
+        if (_coroutine) {
+            _coroutine.resume ();
+            if (_coroutine.done () ) {return end (); }
+        }
+
+        return iterator (_coroutine);
+    }
+
+    iterator end () {return iterator {}; }
+
+private:
+   explicit int_generator (promise_type* coroutine)
+    : _coroutine {std::experimental::coroutine_handle<promise_type>::from_promise(*coroutine) } {}
+
+    std::experimental::coroutine_handle <promise_type> _coroutine;
+
+};
+
+
+int_generator integers (int first, int last)
+{
+    for (auto i {first}; i <= last; ++i) {
+        co_yield i;
+    }
+}
+
+int main ()
+{
+    for (int x: integers (1, 5) ) {
+        std::cout << x << " ";
+    }
+    std::cout << "\n";
+}
